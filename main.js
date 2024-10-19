6239935ca800b5e688337626fd4ba4e684156405
Hamza Saliheddine <hamzasaliheddine@hotmail.com>
2024-10-19 22:43:52 +0100

    need to handle some logics

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ const getIds = async () => {
     );
     let storiesIds = await storiesRes.json();
     let jobsIds = await jobsRes.json();
+    fetchPolls(largestPost);
     fetchStories(storiesIds);
     fetchJobs(jobsIds);
-    fetchPolls(largestPost);
   } catch (err) {
     console.error(err);
   }
@@ -34,6 +34,8 @@ const fetchStories = (data) => {
         by: story.by,
         time: story.time,
         type: story.type,
+        url: story.url,
+        id: story.id,
       });
     } catch (err) {
       console.error(err);
@@ -42,33 +44,40 @@ const fetchStories = (data) => {
 };
 
 const fetchPolls = async (largestPost) => {
-  let i = largestPost;
-  if (i == 0) {
+  let largeID = await largestPost.json();
+  let i = largeID;
+
+  if (i === 0) {
     return;
   }
+
   const fetchNext = async () => {
     try {
       let dataRes = await fetch(
         `https://hacker-news.firebaseio.com/v0/item/${i}.json`
       );
       let poll = await dataRes.json();
-      if (poll.type == "poll") {
+      if (poll && poll.type === "poll") {
         infinity.push({
           text: poll.title,
           by: poll.by,
           time: poll.time,
           type: poll.type,
+          url: poll.url,
+          id: poll.id,
         });
       }
     } catch (err) {
       console.error(err);
     } finally {
       i--;
-      console.log("first loop");
-      setTimeout(fetchNext, 200);
+      if (i >= 0) {
+        setTimeout(fetchNext, 200);
+      }
     }
   };
-  fetchNext();
+
+  await fetchNext();
 };
 
 const fetchJobs = (data) => {
@@ -83,6 +92,8 @@ const fetchJobs = (data) => {
         by: job.by,
         time: job.time,
         job: job.type,
+        url: job.url,
+        id: job.id,
       });
     } catch (err) {
       console.error(err);
@@ -95,20 +106,33 @@ getIds();
 let i = 0;
 let y = 30;
 const displayMore = () => {
-  console.log('clicked')
-  Posts.concat(infinity);
-  while (i+1) {
+  Posts.sort((a, b) => {
+    return b.time - a.time;
+  });
+  while (i + 1) {
     if (i == y) {
       y += 30;
       return;
     }
+    const unixTime = Posts[i].time;
+    const date = new Date(unixTime * 1000);
+
     container.innerHTML += `<div class="post">
     <p>${Posts[i].text}</p>
-    <p>Name: <span id="name">${Posts[i].by}</span></p>
-    <p>Time: <span id="time">${Posts[i].time}</span></p>
+    <a href="${Posts[i].url}" target="_blank" class="url">${Posts[i].url}<a>
+    <p>By: <span id="name">${Posts[i].by}</span></p>
     <p>Type: <span id="type">${Posts[i].type}</span></p>
+    <p>Created at: <span>${date.toLocaleString()}</span></p>
     </div>
     `;
     i++;
   }
 };
+
+const refrechPosts = () => {
+  
+};
+
+setTimeout(() => {
+  displayMore();
+}, 5000);
