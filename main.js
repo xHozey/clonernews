bcca951638ab1a3b296380f057ecb6c7e269424e
hsalihee <you@example.com>
2024-10-19 17:55:12 +0100

    good for now

--- main.js
+++ main.js
@@ -1,98 +1,59 @@
-const stories = document.getElementById("Stories");
-const jobs = document.getElementById("Jobs");
-const polls = document.getElementById("Polls");
-const storieDiv = document.createElement("div");
-const jobDiv = document.createElement("div");
-const pollDiv = document.createElement("div");
-
-const getAllData = async () => {
+let StoriesThread = [];
+let JobsThread = [];
+let PollsThread = [];
+let container = document.getElementById('threads')
+const getIds = async () => {
   try {
-    let response = await Promise.all([
-      fetch("https://hacker-news.firebaseio.com/v0/topstories.json"),
-      fetch("https://hacker-news.firebaseio.com/v0/jobstories.json"),
-    ]);
-
-    let storiesIds = await response[0].json();
-    let jobsIds = await response[1].json();
+    let storiesRes = await fetch(
+      "https://hacker-news.firebaseio.com/v0/topstories.json"
+    );
+    let jobsRes = await fetch(
+      "https://hacker-news.firebaseio.com/v0/jobstories.json"
+    );
+    let storiesIds = await storiesRes.json();
+    let jobsIds = await jobsRes.json();
     fetchStories(storiesIds);
-    fetchPolls(storiesIds);
     fetchJobs(jobsIds);
+    fetchPolls()
   } catch (err) {
     console.error(err);
   }
 };
 
-const getStories = () => {
-  jobs.innerHTML = "";
-  polls.innerHTML = "";
-  stories.append(storieDiv);
-};
-
-const getPolls = () => {
-  jobs.innerHTML = "";
-  stories.innerHTML = "";
-  polls.append(pollDiv);
-};
-
-const getJobs = () => {
-  polls.innerHTML = "";
-  stories.innerHTML = "";
-  jobs.append(jobDiv);
-};
-
 const fetchStories = (data) => {
-  let index = 0; 
-  const interval = setInterval(async () => {
-    if (index >= data.length) {
-      clearInterval(interval); 
-      return;
-    }
-    
-    try {
-      let dataRes = await fetch(
-        `https://hacker-news.firebaseio.com/v0/item/${data[index]}.json`
-      );
-      let job = await dataRes.json();
-      
-      if (job.type === "story") {
-        let li = document.createElement("li");
-        li.classList = "thread";
-        let link = document.createElement("a");
-        link.setAttribute("href", job.url);
-        link.setAttribute("target", "_blank");
-        link.textContent = job.title;
-        storieDiv.append(li); 
-      }
-    } catch (err) {
-      console.error(err);
-    }
-
-    index++; 
-  }, 400);
-};
-
-
-const fetchPolls = (data) => {
   data.forEach(async (id) => {
     try {
       let dataRes = await fetch(
         `https://hacker-news.firebaseio.com/v0/item/${id}.json`
       );
       let job = await dataRes.json();
-      if (job.type == "polls") {
-        let li = document.createElement("li");
-        li.classList = "thread";
-        let link = document.createElement("a");
-        link.setAttribute("href", job.url);
-        link.setAttribute("target", "_blank");
-        link.textContent = job.title;
-        li.appendChild(link);
-        pollDiv.append(li);
-      }
+      StoriesThread.push({ text: job.title, by: job.by, time: job.time })
     } catch (err) {
       console.error(err);
     }
   });
+};
+
+const fetchPolls = async () => {
+  let i = 126809;
+  const fetchNext = async() => {
+    try {
+      let dataRes = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${i}.json`
+      );
+      let job = await dataRes.json();
+      if (job.type == "poll") {
+        PollsThread.push({ text: job.title, by: job.by, time: job.time })
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      i++
+      console.log('first loop')
+      setTimeout(fetchNext, 200)
+    }
+  }
+  fetchNext
 };
 
 const fetchJobs = (data) => {
@@ -102,18 +63,21 @@ const fetchJobs = (data) => {
         `https://hacker-news.firebaseio.com/v0/item/${id}.json`
       );
       let job = await dataRes.json();
-      let li = document.createElement("li");
-      li.classList = "thread";
-      let link = document.createElement("a");
-      link.setAttribute("href", job.url);
-      link.setAttribute("target", "_blank");
-      link.textContent = job.title;
-      li.appendChild(link);
-      jobDiv.append(li);
+      JobsThread.push({ text: job.title, by: job.by, time: job.time })
     } catch (err) {
       console.error(err);
     }
   });
 };
 
-getAllData();
+const displayStories = () => {
+  StoriesThread.forEach((obj) => {
+    
+  })
+}
+
+
+
+
+
+getIds();
