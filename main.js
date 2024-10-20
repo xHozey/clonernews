9500bb7e41f934c24f2ad5b63957070130c22055
hsalihee <you@example.com>
2024-10-20 10:57:44 +0100

    i fixed polls ig

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ const fetchStories = (data) => {
         type: story.type,
         url: story.url,
         id: story.id,
+        comments: story.kids,
       });
     } catch (err) {
       console.error(err);
@@ -47,17 +48,15 @@ const fetchPolls = async (largestPost) => {
   let largeID = await largestPost.json();
   let i = largeID;
 
-  if (i === 0) {
-    return;
-  }
-
   const fetchNext = async () => {
+    if (i === 0) return;
     try {
       let dataRes = await fetch(
         `https://hacker-news.firebaseio.com/v0/item/${i}.json`
       );
       let poll = await dataRes.json();
-      if (poll && poll.type === "poll") {
+
+      if (poll.type === "poll") {
         infinity.push({
           text: poll.title,
           by: poll.by,
@@ -65,19 +64,19 @@ const fetchPolls = async (largestPost) => {
           type: poll.type,
           url: poll.url,
           id: poll.id,
+          comments: poll.kids,
         });
+        //updates
       }
     } catch (err) {
       console.error(err);
     } finally {
       i--;
-      if (i >= 0) {
-        setTimeout(fetchNext, 200);
-      }
+      setTimeout(fetchNext, 200);
     }
   };
 
-  await fetchNext();
+  fetchNext();
 };
 
 const fetchJobs = (data) => {
@@ -94,6 +93,7 @@ const fetchJobs = (data) => {
         job: job.type,
         url: job.url,
         id: job.id,
+        comments: job.kids,
       });
     } catch (err) {
       console.error(err);
@@ -105,7 +105,7 @@ getIds();
 
 let i = 0;
 let y = 30;
-const displayMore = () => {
+const displayMore = async () => {
   Posts.sort((a, b) => {
     return b.time - a.time;
   });
@@ -117,21 +117,56 @@ const displayMore = () => {
     const unixTime = Posts[i].time;
     const date = new Date(unixTime * 1000);
 
-    container.innerHTML += `<div class="post">
+    container.innerHTML += `<div class="post" id="${Posts[i].id}">
     <p>${Posts[i].text}</p>
-    <a href="${Posts[i].url}" target="_blank" class="url">${Posts[i].url}<a>
+    <a href="${Posts[i].url ? Posts[i].url : ""}" target="_blank" class="url">${
+      Posts[i].url ? Posts[i].url : ""
+    }<a>
     <p>By: <span id="name">${Posts[i].by}</span></p>
     <p>Type: <span id="type">${Posts[i].type}</span></p>
     <p>Created at: <span>${date.toLocaleString()}</span></p>
     </div>
     `;
+    // let comments = await getComments(Posts[i].kids)
+    // for (let cmt of comments) {
+    //   let parent = document.getElementById(Posts[i].id)
+    //   parent.innerHTML += `<div class="comment">
+    //   <p>${cmt.text}</p>
+    //   <p>by: ${cmt.by}</p>
+    //   <p>Created at: ${cmt.date}</p>
+    //   </div>`
+    // }
     i++;
   }
 };
 
-const refrechPosts = () => {
-  
+const getComments = async (kids) => {
+  let comments = [];
+  for (let i of kids) {
+    try {
+      let commentRes = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${i}.json`
+      );
+      let comment = await commentRes.json();
+      const unixTime = comment.time;
+      const date = new Date(unixTime * 1000);
+      comments.push({
+        user: comment.by,
+        text: comment.text,
+        date: date.toLocaleString(),
+        unixTime: comment.time,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  comments.sort((a, b) => {
+    return b.unixTime - a.unixTime;
+  });
+  return comments;
 };
+
+const refrechPosts = () => {};
 
 setTimeout(() => {
   displayMore();
